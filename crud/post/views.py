19c9a1f9e5c239aafeb03019a15c45868d58acc4
givamarques <givaldosantos@live.com>
2021-05-03 23:27:01 -0300

    03 ok

--- crud/post/views.py
+++ crud/post/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from .models import Post
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html')
+    posts=Post.objects.all()
+    return render(request, 'home.html',{'posts':posts})
 
 # Add method
 def add(request):
+    if request.method=='POST':
+        title=request.POST['title']
+        detail=request.POST['detail']
+        Post.objects.create(title=title,detail=detail)
+        messages.success(request, 'Data has been added')
     return render(request, 'add.html')
 
 # Update method
